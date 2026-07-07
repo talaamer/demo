7250ca04c6443f624e17cb0963ce924bb66976a4
Demo <demo.gmail.com>
2026-07-07 12:20:24 +0300

    Init commit

---
+++ src/test/java/com/bibf/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
